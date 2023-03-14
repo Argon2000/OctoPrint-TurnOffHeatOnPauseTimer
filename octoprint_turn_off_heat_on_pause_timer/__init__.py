aad0d4430a6712e9e82cdc801338a74f4c980ff8
Per Åstrand <per.astrand@addovation.com>
2023-03-14 15:56:40 +0100

    Wait for temperatures

--- octoprint_turn_off_heat_on_pause_timer/__init__.py
+++ octoprint_turn_off_heat_on_pause_timer/__init__.py
@@ -4,6 +4,7 @@ from __future__ import absolute_import
 import octoprint.plugin
 from octoprint.events import Events
 from octoprint.util import ResettableTimer
+from octoprint.util import RepeatedTimer
 
 class TurnOffHeatOnPauseTimerPlugin(
     octoprint.plugin.StartupPlugin,
@@ -13,9 +14,11 @@ class TurnOffHeatOnPauseTimerPlugin(
     octoprint.plugin.EventHandlerPlugin
 ):
     def __init__(self):
-        self.timer = None
+        self.stop_timer = None
+        self.start_timer = None
         self.printer_profile = None
-        self.lastTemps = None
+        self.last_temps = None
+        self.paused_by_plugin = False
 
     # ~~ StartupPlugin mixin
     def on_after_startup(self):
@@ -47,32 +50,40 @@ class TurnOffHeatOnPauseTimerPlugin(
 
     # ~~ EventHandlerPlugin mixin
     def on_event(self, event, payload):
-        if event == Events.PRINT_RESUMED and self.lastTemps != None:
-            self._logger.info("Turn off heat on pause timer: Restoring temperatures and resuming")
-            for k in self.lastTemps.keys():
-                self._printer.set_temperature(k, self.lastTemps[k])
-            self.timer.cancel()
+        if event == Events.PRINT_RESUMED and self.last_temps != None:
+            self._logger.info("Turn off heat on pause timer: Restoring temperatures, waiting and then resuming")
+            for k in self.last_temps.keys():
+                self._printer.set_temperature(k, self.last_temps[k])
+            if self.stop_timer != None:
+                self.stop_timer.cancel()
+            self.start_timer = RepeatedTimer(1, self.start_on_temperatures_restored)
+            self.start_timer.start()
+            self.paused_by_plugin = True
+            self._printer.pause_print()
         if event == Events.PRINT_PAUSED:
-            self.lastTemps = dict()
+            if self.paused_by_plugin:
+                self.paused_by_plugin = False
+                return
+            self.last_temps = dict()
             temps = self._printer.get_current_temperatures()
             shut_off_heatbed = self._settings.get_float(["shut_off_heatbed"])
             shut_off_hotend = self._settings.get_float(["shut_off_hotend"])
             shut_off_heated_chamber = self._settings.get_float(["shut_off_heated_chamber"])
             for k in temps.keys():
                 if ("tool" in k) and shut_off_hotend:
-                    self.lastTemps[k] = temps[k]["target"]
+                    self.last_temps[k] = temps[k]["target"]
                 if k == "bed" and shut_off_heatbed:
-                    self.lastTemps[k] = temps[k]["target"]
+                    self.last_temps[k] = temps[k]["target"]
                 if k == "chamber" and shut_off_heated_chamber:
-                    self.lastTemps[k] = temps[k]["target"]
+                    self.last_temps[k] = temps[k]["target"]
             time_in_seconds = self._settings.get_float(["timer_time_in_seconds"])
             self._logger.info("Turn off heat on pause timer started! Will stop after {}".format(time_in_seconds))
-            self.timer = ResettableTimer(time_in_seconds, self.turn_off_heat)
-            self.timer.start()
+            self.stop_timer = ResettableTimer(time_in_seconds, self.turn_off_heat)
+            self.stop_timer.start()
 
     def turn_off_heat(self):
         printer = self._printer
-        self.timer = None
+        self.stop_timer = None
         if printer.is_paused():
             shut_off_heatbed = self._settings.get_float(["shut_off_heatbed"])
             shut_off_hotend = self._settings.get_float(["shut_off_hotend"])
@@ -87,6 +98,20 @@ class TurnOffHeatOnPauseTimerPlugin(
                 if k == "chamber" and shut_off_heated_chamber:
                     self._logger.info("Turn off heat on pause timer: Turning off heated chamber")
                     self._printer.set_temperature(k, 0)
+
+    def start_on_temperatures_restored(self):
+        printer = self._printer
+        if printer.is_paused() and self.last_temps != None:
+            current_temps = self._printer.get_current_temperatures()
+            for k in current_temps.keys():
+                if k in self.last_temps.keys():
+                    current = current_temps[k]["actual"]
+                    target = current_temps[k]["target"]
+                    if current < target:
+                        return
+            self.start_timer.cancel()
+            self.start_timer = None
+            printer.resume_print()
 
     # ~~ Softwareupdate hook
     def get_update_information(self):
