9488f5a99fb744110d1b545a73ca6287b258d7e5
Per Åstrand <per.astrand@addovation.com>
2023-03-14 14:44:29 +0100

    Set printer profile for use later

--- octoprint_turn_off_heat_on_pause_timer/__init__.py
+++ octoprint_turn_off_heat_on_pause_timer/__init__.py
@@ -14,6 +14,7 @@ class TurnOffHeatOnPauseTimerPlugin(
 ):
     def __init__(self):
         self.timer = None
+        self.printer_profile = None
 
     # ~~ StartupPlugin mixin
     def on_after_startup(self):
@@ -57,10 +58,10 @@ class TurnOffHeatOnPauseTimerPlugin(
             shut_off_heatbed = self._settings.get_float(["shut_off_heatbed"])
             shut_off_hotend = self._settings.get_float(["shut_off_hotend"])
             if shut_off_hotend:
-                for i in range(printer.profile.extruder.count):
+                for i in range(self.printer_profile.extruder.count):
                     self._logger.info("Turn off heat on pause timer: Turning off hotend{}".format(i+1))
                     printer.set_temperature("tool{}".format(i), 0)
-            if shut_off_heatbed:
+            if shut_off_heatbed and self.printer_profile.heatedBed:
                 self._logger.info("Turn off heat on pause timer: Turning off heatbed")
                 printer.set_temperature("bed", 0)
 
@@ -81,6 +82,10 @@ class TurnOffHeatOnPauseTimerPlugin(
                 "pip": "https://github.com/Argon2000/OctoPrint-TurnOffHeatOnPauseTimer/archive/{target_version}.zip",
             }
         }
+    
+    def createFilePreProcessor(self, path, file_object, blinks=None, printer_profile=None, allow_overwrite=True, *args, **kwargs):
+        self._logger.info("Turn off heat on pause timer: Setting printer profile")
+        self.printer_profile = printer_profile
 
 __plugin_name__ = "Turn off heat on pause timer"
 __plugin_version__ = "0.0.1"
@@ -93,5 +98,6 @@ def __plugin_load__():
 
     global __plugin_hooks__
     __plugin_hooks__ = {
+        "octoprint.filemanager.preprocessor": __plugin_implementation__.set_printer_profile,
         "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
     }
